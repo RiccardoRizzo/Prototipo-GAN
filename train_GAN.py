e0d2ba98ebdede4cb9a529f36f01537b5c20b3b4
Riccardo Rizzo <riccardo.rizzo@icar.cnr.it>
2020-07-19 12:38:16 +0200

    si aggiunge la cadenza di salvataggio dei modelli

--- train_GAN.py
+++ train_GAN.py
@@ -74,15 +74,16 @@ def saveFakeImages(fake, nomefile):
     img2 = Image.fromarray(img2)
     img2.save(nomefile)
 
+def salvaImmagini(nomeDir, nomeFile, fixed_noise):
+    with torch.no_grad():
+            fake = netG(fixed_noise).detach().cpu()
+            nomeFileImage = os.path.join(nomeDir, nomeFile +".jpg")
+            saveFakeImages(fake, nomeFileImage)
+
 
 #---------------------------------------------------
 def salvaCheckpoint(nomeDir, nomeFile, netD, netG, optimizerD, optimizerG, fixed_noise):
-    with torch.no_grad():
-        fake = netG(fixed_noise).detach().cpu()
-
-    nomeFileImage = os.path.join(nomeDir, nomeFile +".jpg")
-    saveFakeImages(fake, nomeFileImage)
-
+    
     nomeCheckpoint = os.path.join(nomeDir, nomeFile +"__D.pth" )
     torch.save({
             'model_state_dict': netD.state_dict(),
@@ -260,8 +261,10 @@ def main(pl, paramFile):
         # Fine dell'epoca --------------------------------------
         # salva un provino delle immagini generate ed i modelli relativi
         nomeFile = pl["nomeModello"]+ "_" +str(epoch)
-        # salva i modelli ogni 10 epoche
-        if epoch %10 == 0:
+
+        salvaImmagini(nomeDir, nomeFile, fixed_noise)
+        # salva i modelli ogni cadenza_epoche   
+        if epoch % pl["cadenza_epoche"] == 0:
             salvaCheckpoint(nomeDir, nomeFile, netD, netG, optimizerD, optimizerG, fixed_noise)
 
     # Fine del training =======================================
